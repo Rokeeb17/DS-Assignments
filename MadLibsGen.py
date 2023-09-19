034f8586c34c31e5833ac036f593fcad76bcf83f
Rokeeb17 <rokeeb02@gmail.com>
2023-09-19 05:23:10 +0100

    TextAdv assignment
    Almost complete

---
+++ MadLibsGen.py
@@ -0,0 +1,31 @@
+""" 
+Project Brief
+Inspired by Summer Son’s Mad Libs project with Javascript. The program will first prompt the
+user for a series of inputs a la Mad Libs. For example, a singular noun, an adjective, etc. Then,
+once all the information has been inputted, the program will take that data and place them into a
+premade story template. You’ll need prompts for user input, and to then print out the full story at
+the end with the input included.
+Concepts to keep in mind:
+● Strings
+● Variables
+● Concatenation
+● Print 
+
+Deliverables
+A pretty fun beginning project that gets you thinking about how to manipulate user inputted data.
+Compared to the prior projects, this project focuses far more on strings and concatenating.
+Have some fun coming up with some wacky stories for this!
+"""
+
+#let's get a story
+"""I went to the shop and bought apples. While I was there I saw Ricky Gervais. He gave me a 
+bike and told me to go downtown, to the bad neighbourhood. I got there and was attacked by tigers 
+gorillas"""
+
+print('Let the game begin!')
+name = input('Enter a name: ')
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+celeb = input('Enter a celeb name: ')
+
+
